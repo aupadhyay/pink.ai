d3d52ce3b383eea7debb44dc9e9627b13a75f630
Abhi Upadhyay <abhi.upadhyay@berkeley.edu>
2020-03-23 04:19:29 -0700

    Added explanations & treatments for types

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var images = [];
 fs.readdir(path.join(__dirname, 'static', 'img', 'benign'), (err, files) => {
 	files.forEach(function(file){
 		images.push({
-			"url": `img/benign/${file}`, 
+			"url": `img/benign/${file}`,
 			"class": "benign"
 		});
 	})
@@ -18,7 +18,7 @@ fs.readdir(path.join(__dirname, 'static', 'img', 'benign'), (err, files) => {
 fs.readdir(path.join(__dirname, 'static', 'img', 'malignant'), (err, files) => {
 	files.forEach(function(file){
 		images.push({
-			"url": `img/malignant/${file}`, 
+			"url": `img/malignant/${file}`,
 			"class": "malignant"
 		});
 	})
@@ -65,17 +65,45 @@ const typeStrings = {
 	'PC': 'Papillary Carcinoma',
 };
 
+const treatments = {
+	'benign': [],
+	'DC': ['Lumpectomy', 'Mastectomy', 'Chemotherapy', 'Radiation Therapy', 'Hormonal Therapy'],
+	'LC': ['Surgery', 'Chemotherapy', 'Radiation Therapy', 'Hormonal Therapy', 'Targeted Therapy'],
+	'MC': ['Lumpectomy', 'Chemotherapy', 'Radiation Therapy', 'Hormonal Therapy', 'Mastectomy'],
+	'PC': ['Surgery', 'Chemotherapy', 'Radiation Therapy', 'Hormonal Therapy', 'Targeted Therapy']
+};
+
+const explanations = {
+	'benign': '',
+	'DC': 'Ductal carcinoma is a common type of breast cancer that starts in cells in milk ducts.\
+		   This type accounts for 80% of invasive breast cancer in women and 90% in men. \
+		   <a href="https://www.webmd.com/breast-cancer/ductal-carcinoma-invasive-in-situ#1">More Info</a>',
+	'LC': 'Lobular carcinoma is the second most common type of breast cancer and starts in the lobules. \
+		   It\'s the most likely to be found in both breasts out of all the types. \
+		   <a href="https://www.webmd.com/breast-cancer/lobular-carcinoma-invasive-and-in-situ#1">More Info</a>',
+	'MC': 'Mucinous tumors are rare forms of ductal carcinoma and account for 2-3% of breast cancer cases. \
+		   The tumor is made up of cells that float in mucin. \
+		   <a href="https://www.breastcancer.org/symptoms/types/mucinous">More Info</a>',
+	'PC': 'Papillary tumors are rare and account for 2% of breast cancer cases. They are diagnosed in \
+		   older women who have already been through menopause. \
+		   <a href="https://www.breastcancer.org/symptoms/types/papillary">More Info</a>'
+};
+
 app.get('/results/:id', function(req, res) {
 	var id = req.params.id;
 	var json = JSON.parse(Buffer.from(id, 'base64').toString());
 	var benign_pred = json.type == "benign";
 	json.confidence = parseFloat((json.confidence * 100).toString().slice(0, 5));
 	json.classification = benign_pred ? "BENIGN" : "MALIGNANT";
+	json.treatments = treatments[json.type];
+	json.explanation = explanations[json.type];
 	json.type = typeStrings[json.type];
 
+	var uploaded = json.file_path.substr(0, 3) == "SOB";
 	var benign_actual = json.file_path.substr(4, 1) == "b";
-	json.expected_class =  benign_actual ? "BENIGN" : "MALIGNANT"
-	json.expected_type = benign_actual ? "" : typeStrings[json.file_path.substr(20, 2)];
+	json.expected_class =  benign_actual ? "BENIGN" : "MALIGNANT";
+	if (uploaded) json.expected_class = "N/A";
+	json.expected_type = (benign_actual || uploaded) ? "" : typeStrings[json.file_path.substr(20, 2)];
 	res.render('results', json);
 });
 
